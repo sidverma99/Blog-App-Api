c859c07d83875238f82604f17108f977772eced7
Siddharth Verma <60062454+sidverma99@users.noreply.github.com>
2022-05-08 08:52:25 +0530

    Api for Blog application

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose=require('mongoose');
+
+const UserSchema=new mongoose.Schema({
+    username:{
+        type: String,
+        unique: true,
+        required: true,
+    },
+    email:{
+        type: String,
+        unique: true,
+        required: true,
+    }, 
+    password: {
+        type: String,
+        required: true,
+    },
+    profilePic: {
+        type: String,
+        default: "",
+    },
+}, 
+{timestamps: true}
+);
+
+module.exports=mongoose.model("User",UserSchema);
